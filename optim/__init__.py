3a280717727257cfb3c382d5a8006d6067271ab6
RanchiZhao <yuanqian_zhao@163.com>
2023-07-05 15:30:12 +0800

    qlora

--- optim/__init__.py
+++ optim/__init__.py
@@ -1,4 +1,4 @@
 from .adam import AdamOptimizer
 from .adam_offload import AdamOffloadOptimizer
 from .optim_manager import OptimManager
-from .adam_8bit import Adam8bit,PagedAdamW32bit
+from .adam_8bit import Adam8bit,PagedAdamW32bit,PagedAdamW8bit
